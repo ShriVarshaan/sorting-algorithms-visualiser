cb1a1f081e6551f241389367309fc2bd3d562b09
shri varshaan <varshaanvijay@gmail.com>
2025-06-30 04:45:22 +0530

    missing controls displayed

--- Main.py
+++ Main.py
@@ -48,10 +48,10 @@ def draw(draw_info, algo_name, ascending):
     title = draw_info.LARGE_FONT.render(f"{algo_name}, {ascending}", 1,draw_info.BLACK)  # the 1 is essentially the sharpness of the line
     draw_info.window.blit(title, (draw_info.width / 2 - title.get_width() / 2, 0))
 
-    controls = draw_info.FONT.render("R - Reset | Space - Start Sorting | A - Ascending | D - descending", 1, draw_info.BLACK) #the 1 is essentially the sharpness of the line
+    controls = draw_info.FONT.render("R - Reset | Space - Start Sorting | A - Ascending | D - Descending", 1, draw_info.BLACK) #the 1 is essentially the sharpness of the line
     draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2,45))
 
-    sorting = draw_info.FONT.render("B - Bubble Sort | I - Insertion Sort | Q - Quick sort", 1, draw_info.BLACK)
+    sorting = draw_info.FONT.render("B - Bubble sort | I - Insertion sort | Q - Quick sort | M - Merge sort", 1, draw_info.BLACK)
     draw_info.window.blit(sorting, (draw_info.width / 2 - sorting.get_width() / 2, 75))
 
     draw_list(draw_info)
